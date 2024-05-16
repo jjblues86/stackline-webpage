a02f40ec64513e5df01c6230b77b1eaeb4948c07
Jerome Joof <jeromejoof@yahoo.com>
2024-05-16 10:31:51 -0700

    Made changes to the graph.

--- stackline-webpage/src/Sales/SalesGraph.tsx
+++ stackline-webpage/src/Sales/SalesGraph.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 import { SalesData } from "../types/chartDataTypes";
 import { GraphContainer } from "./SalesGraphStyles";
 
